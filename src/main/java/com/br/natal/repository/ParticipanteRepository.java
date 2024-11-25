fa3f5f8f768ac186b7c75ee335cae7a37f605d1c
LucaasHenrique <lucashsouza101@gmail.com>
2024-11-25 18:10:16 -0300

    repository

--- src/main/java/com/br/natal/repository/ParticipanteRepository.java
+++ src/main/java/com/br/natal/repository/ParticipanteRepository.java
@@ -1,11 +1,17 @@
 package com.br.natal.repository;
 
+import com.br.natal.entity.Grupo;
 import com.br.natal.entity.Participante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
 
     Boolean existsByNome(String nome);
+    List<Participante> findByGrupoId(Long id);
+
+    Participante findParticipanteById(Long id);
 }
